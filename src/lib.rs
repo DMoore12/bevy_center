54e6975113461000026c3a79696cc0b019924a82
Dawson Moore <dwmm127@gmail.com>
2024-10-20 01:02:27 -0500

    Test workflows again...

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 /// Center marker plugin
 pub mod center_marker;
 
-/// Commonly used stuff
+/// Commonly used things
 pub mod prelude {
     #[doc(hidden)]
     pub use crate::center_marker::{CenterEntity, CenterMarkerPlugin, despawn_center_system};
